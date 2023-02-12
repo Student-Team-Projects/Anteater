1dabb885f2bb5fc9fa54ca6f8af987747866ba0b
Tomasz Mazur <mazur.tomasz34@gmail.com>
2023-02-12 17:59:01 +0100

    Updated BPF code license to GPL

--- include/utils.bpf.h
+++ include/utils.bpf.h
@@ -9,4 +9,4 @@
 
 // time of last boot, injected by user
 const volatile __weak uint64_t boot_time;
-char LICENSE[] __weak SEC("license") = "Dual BSD/GPL";
+char LICENSE[] __weak SEC("license") = "GPL";
