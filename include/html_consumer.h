340d76c9d6aba263d8fe40bd3be063c6b3946f73
katzper.michno <katzper.michno@gmail.com>
2023-02-20 22:53:36 +0100

    implemented html consumer

--- include/html_consumer.h
+++ include/html_consumer.h
@@ -1,18 +1,31 @@
 #pragma once
 
+#include <chrono>
+
 #include "consumer.h"
 #include "provider.h"
+
+#include "html/HtmlViewFactory.h"
+#include "presentation/MainPresenter.h"
+#include "presentation/PresenterFactory.h"
 
 class HtmlConsumer : public Consumer {
     pid_t root_pid;
     volatile bool exiting = false;
     bool hex_input;
 
+    /* Objects from frontend module */
+    std::unique_ptr<HtmlViewFactory> view_factory;
+    std::unique_ptr<PresenterFactory> presenter_factory;
+    std::unique_ptr<MainPresenter> main_presenter;
+
 public:
     HtmlConsumer() = default;
     int start(Provider &provider, pid_t root_pid, bool hex_input) override;
     void stop() override;
 
-private: 
+private:
+    /* Converts timestamp from epoch */
+    std::chrono::system_clock::time_point toSystemClockTimePoint(uint64_t event_timestamp);
     int consume(Provider &provider);
 };
--- src/html_consumer.cpp
+++ src/html_consumer.cpp
@@ -3,12 +3,21 @@
 
 #include "spdlog/spdlog.h"
 
+#include <chrono>
 #include <cstdio>
-#include <iostream>
+
+#include "html/HtmlViewFactory.h"
+#include "presentation/MainPresenter.h"
+#include "presentation/PresenterFactory.h"
 
 int HtmlConsumer::start(Provider &provider, pid_t root_pid, bool hex_input) {
     this->root_pid = root_pid;
     this->hex_input = hex_input;
+
+    const std::filesystem::path directory = "./html/";
+    this->view_factory = std::make_unique<HtmlViewFactory>(directory, "index", "styles", true);
+    this->presenter_factory = std::make_unique<PresenterFactory>(std::move(view_factory));
+    this->main_presenter = presenter_factory->createMainPresenter();
 
     SPDLOG_INFO("Starting html consumer");
     
@@ -17,53 +26,52 @@ int HtmlConsumer::start(Provider &provider, pid_t root_pid, bool hex_input) {
     return 0;
 }
 
+std::chrono::system_clock::time_point HtmlConsumer::toSystemClockTimePoint(uint64_t event_timestamp) {
+    using namespace std::chrono;
+    auto duration_ns = duration<uint64_t, std::nano>(event_timestamp);
+    return system_clock::time_point(duration_cast<system_clock::duration>(duration_ns));
+}
+
 int HtmlConsumer::consume(Provider &provider) {
-    Provider::event_ref e = provider.provide();
+    Provider::event_ref event = provider.provide();
 
     if (exiting)
         return 0;
 
-    if (!e) {
+    if (!event) {
         SPDLOG_ERROR("Got null pointer from provider. Stopping consuming");
         stop();
         return 1;
     }
 
-    switch (e->event_type) {
+    auto timestamp = toSystemClockTimePoint(event->timestamp);
+
+    switch (event->event_type) {
         case FORK:
-            printf("%-20lu: %-8s %-7u %-13u\n", e->timestamp, "FORK", e->fork.child_pid, e->pid);
+            main_presenter->addForkEvent(timestamp, event->pid, event->fork.child_pid);
             return 0;
         case EXIT:
-            printf("%-20lu: %-8s %-7u %-13u\n", e->timestamp, "EXIT", e->pid, e->exit.code);
-            if (e->pid == root_pid) {
+            main_presenter->addExitEvent(timestamp, event->pid, event->exit.code);
+            if (event->pid == root_pid) {
                 stop();
                 provider.stop();
             }
             return 0;
         case WRITE:
-            if (e->write.length > 0) 
-                printf(
-                    "%-20lu: %-8s %-7u %-8s %.*s\n", 
-                    e->timestamp,
-                    "WRITE", 
-                    e->pid,
-		    (e->write.stream == STDOUT) ? "STDOUT" : "STDERR",
-                    static_cast<int>(e->write.length), 
-                    (hex_input) ? hex_to_string(e->write.data, e->write.length).data() : e->write.data
-                );
+            main_presenter->addWriteEvent(
+                timestamp,
+                event->pid,
+                std::string_view(event->write.data, event->write.length),
+                event->write.stream == Stream::STDOUT
+            );
             return 0;
         case EXEC:
-            convert_spaces(e->exec.data, e->exec.length);
-            if (e->exec.length > 0) 
-                printf(
-                    "%-20lu: %-8s %-7u %-7u %.*s\n",
-                    e->timestamp,
-                    "EXEC",
-                    e->pid,
-                    e->exec.uid,
-                    static_cast<int>(e->exec.length),
-                    (hex_input) ? hex_to_string(e->exec.data, e->exec.length).data() : e->exec.data
-                );
+            main_presenter->addExecEvent(
+                timestamp,
+                event->pid,
+                event->exec.uid,
+                std::string_view(event->exec.data, event->exec.length)
+            );
             return 0;
         default:
             stop();
